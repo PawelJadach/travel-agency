9b63e381f45c541c5ef7ece7798c6364daffc2f2
unknown <pawel1234477@wp.pl>
2019-12-18 07:40:30 +0100

    filters not work

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,11 +11,13 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
-
+export const CHANGE_DURATION_FROM = createActionName('CHANGE_DURATION_FROM');
+export const CHANGE_DURATION_TO = createActionName('CHANGE_DURATION_TO');
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
-
+export const changeDurationFrom = (value) => ({ value , type: CHANGE_DURATION_FROM});
+export const changeDurationTo = (value) => ({ value , type: CHANGE_DURATION_TO});
 // reducer
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
@@ -25,6 +27,18 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case CHANGE_DURATION_FROM:
+      //console.log(action);
+      return {
+        ...statePart,
+        from: action.value,
+      };
+    case CHANGE_DURATION_TO:
+      console.log(action.value);
+      return {
+        ...statePart,
+        to: action.value,
+      };
     default:
       return statePart;
   }
