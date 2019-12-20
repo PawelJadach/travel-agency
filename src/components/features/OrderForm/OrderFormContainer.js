8fd31f8319969de8e4f53923e58a6434bfda6ae6
Paweł Jadach <pawel.jadach.96@gmail.com>
2019-12-20 18:11:50 +0100

    add dropdown work

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -4,9 +4,13 @@ import { getOrderOptions, setOrderOption } from '../../../redux/orderRedux';
 
 const mapStateToProps = state => ({
   options: getOrderOptions(state),
-  setOrderOption: setOrderOption(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  setOrderOption: option => dispatch(setOrderOption(option)),
+
 });
 
 
 
-export default connect(mapStateToProps)(OrderForm);
+export default connect(mapStateToProps, mapDispatchToProps)(OrderForm);
