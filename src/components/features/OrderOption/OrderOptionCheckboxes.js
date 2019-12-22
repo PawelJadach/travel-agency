f55ecc201c4e73df2615a85f3c1a532b3b166987
unknown <pawel1234477@wp.pl>
2019-12-22 10:41:24 +0100

    add ordreroptioncechkboxes

--- src/components/features/OrderOption/OrderOptionCheckboxes.js
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+import formatPrice from '../../../utils/formatPrice';
 
-const OrderOptionCheckboxes = () => {
+const newValueSet = (currentValue, id, checked) => {
+  if(checked){
+    return [
+      ...currentValue,
+      id,
+    ];
+  } else {
+    return currentValue.filter(value => value != id);
+  }
+};
+
+const OrderOptionCheckboxes = (props) => {
+  console.log(props);
   return (
-    <div>
-      <p>OrderOptionCheckboxes</p>
+    <div className={styles.checkboxes}>
+      {props.values.map(value => {
+        return (
+          <label key={value.id}>
+            <input type='checkbox' value={value.id} checked={props.currentValue.indexOf(value.id) >= 0} onChange={()=> {
+              props.setOptionValue(newValueSet(props.currentValue, value.id, props.currentValue.indexOf(value.id) < 0));
+            }}/>
+            {value.name}
+          ({formatPrice(value.price)})
+          </label>
+        );
+      })}
     </div>
   );
+};
+
+OrderOptionCheckboxes.propTypes = {
+  values: PropTypes.array,
+  currentValue: PropTypes.array,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionCheckboxes;
