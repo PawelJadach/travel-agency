21f42b763dca80699b406fb58f38f9087f3d3362
PawelJadach <pawel.jadach.96@gmail.com>
2020-01-22 07:02:26 +0100

    second test for HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import styles from './HappyHourAd.scss';
 
 class HappyHourAd extends Component {
 
   render (){
     return (
-            <>
-            </>
+      <>
+        <h3 className={styles.title}></h3>
+        <div className={styles.promoDescription}></div>
+      </>
     );
   }
 }
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -3,9 +3,24 @@ import { shallow } from 'enzyme';
 import React from 'react';
 
 
+const select = {
+  title: '.title',
+  promo: '.promoDescription',
+  h3: '.coutdown',
+};
+
 describe('Component HappyHourAd', () => {
   it('shuld render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+
+  it('shuld render title and countdown', () => {
+    const component = shallow(<HappyHourAd />);
+    //   const subcomponent = component.find('div');
+    //   const renderedSubcomponent = subcomponent.dive();
+
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promo)).toEqual(true);
   });
 });
